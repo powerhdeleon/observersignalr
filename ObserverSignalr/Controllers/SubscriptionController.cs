09ad5bfd2f9e18b48769b272be9979fd4d84af7f
hdeleon <hdeleon@bioxor.com>
2021-03-27 13:01:04 -0600

    Agregar archivos de proyecto.

---
+++ ObserverSignalr/Controllers/SubscriptionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObserverSignalr.Controllers
+{
+    public class SubscriptionController : Controller
+    {
+        public static List<int> account = new List<int>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Followers(string room)
+        {
+            return View("Followers",room);
+        }
+    }
+}
---
+++ ObserverSignalr/SubscriptionHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObserverSignalr
+{
+    public class SubscriptionHub : Hub
+    {
+        public async Task SendMessage(string room, string message)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+
+            await Clients.Group(room).SendAsync("ReceiveMessage", message);
+        }
+
+        public async Task AddToGroup(string room)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+
+            await Clients.All
+                .SendAsync("ShowWho", $"{Context.ConnectionId} se ha unido al grupo {room}.");
+        }
+
+        public async Task RemoveFromGroup(string room)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, room);
+
+            await Clients.Group(room)
+                .SendAsync("ShowWho", $"{Context.ConnectionId} salio del grupo {room}.");
+        }
+    }
+}
